dabdc3e939c4d3f7ac172a2ff6e3c7f103abc85c
unknown <ummayjannatsadia@gmail.com>
2025-01-28 22:27:08 +0600

    navbar

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,18 +1,22 @@
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import "./Navbar.css"
 const Navbar = () => {
-  const navlink = <div>
-  <Link to ="/">Home</Link>
-  <Link to ="about">About</Link>
-  <Link to ="appoinment">Appoinment</Link>
-  <Link to ="contact">Contact</Link>
-  <Link to ="reviews">Reviews</Link>
-
-  </div>
   return (
-   <div>
-    {navlink}
-   </div>
+   <nav className="nav container">
+     <input type="checkbox" id="check"/>
+            <label for="check">
+                <i class="fa-solid fa-bars" id="open"></i>
+                <i class="fa-solid fa-xmark" id="close"></i>
+            </label>
+    <h4>Doctors Portal</h4>
+  <ul className="">
+  <li><NavLink to ="/">Home</NavLink></li>
+  <li><NavLink to ="about">About</NavLink></li>
+  <li><NavLink to ="appoinment">Appoinment</NavLink></li>
+  <li><NavLink to ="contact">Contact</NavLink></li>
+ <li> <NavLink to ="reviews">Reviews</NavLink></li>
+  </ul>
+   </nav>
   );
 };
 
